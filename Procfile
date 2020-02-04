9bf8ee0b0703b025eb5fcd9fbfd0ba1220b38af9
Nathan Sariowan <ncsariowan@gmail.com>
2020-02-04 15:30:47 -0800

    edit things

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
 web: gunicorn poetryai:app 
-init: flask db init
-migrate: flask db migrate
-upgrade: flask db upgrade
--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_heroku import Heroku
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+heroku = Heroku(app)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
